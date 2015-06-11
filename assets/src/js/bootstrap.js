f9e9e62a8575ff396ee65a9b293fc051be785d59
jvaneeden <jim_v_eeden@hotmail.com>
2015-06-11 15:54:11 +0200

    now reads data url from inside iframe

--- assets/src/js/bootstrap.js
+++ assets/src/js/bootstrap.js
@@ -8,21 +8,22 @@
 
     // Wraps the game for use on TheAIGames website
     // Takes care of setting up and destroying the competition namespace
-    // AIGames.util.legacyWrapper(TetrisGame, {
-    game = new TetrisGame({
-        name: 'tetris-game',
-        player: {
-            // Determines whether they player's chrome should be displayed
-            chrome: true,
-            // Determines whether view selection should be possible
-            viewstack: false,
-            // A number between 0 and 1
-            aspectRatio: 1000 / 558,
-            // Time between each step when playing
-            playbackTimeout: {
-                min: 30,
-                max: 850
-            },
-        }
-    });
+    AIGames.util.legacyWrapper(TetrisGame,
+        new TetrisGame({
+            name: 'tetris-battle',
+            player: {
+                // Determines whether they player's chrome should be displayed
+                chrome: true,
+                // Determines whether view selection should be possible
+                viewstack: false,
+                // A number between 0 and 1
+                aspectRatio: 1000 / 558,
+                // Time between each step when playing
+                playbackTimeout: {
+                    min: 30,
+                    max: 850
+                },
+            }
+        })
+    );
 }());
